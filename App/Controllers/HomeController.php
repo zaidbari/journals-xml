64f265a24c41f26beec63e28d46b28a43d07474f
Zaid Bari <zaidbari9@hotmail.com>
2023-10-17 16:40:17 +0300

    home controller

--- App/Controllers/HomeController.php
+++ App/Controllers/HomeController.php
@@ -7,6 +7,9 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $journal_data = $this->db()->table('settings')->select()->first();
+        $featured_article = $this->db()->table('featuredarticle')->select()->first();
+        $editorial_board = $this->db()->table('editors')->select()->get();
 
         $this->view('home/index', [
             "meta" => [
@@ -14,9 +17,9 @@ class HomeController extends Controller
                 "description" => "Home page",
                 "keywords" => "home, page"
             ],
-            "journal_data" => [],
-            "featured_article" => [],
-            "editorial_board" => [],
+            "journal_data" => $journal_data,
+            "featured_article" => $featured_article,
+            "editorial_board" => $editorial_board,
             "most_read_articles" => [],
             "most_cited_articles" => [],
             "most_downloaded_articles" => [],
